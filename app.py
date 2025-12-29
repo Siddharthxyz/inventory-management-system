8eae6d32ace64cde2427c2fcd5cf64d2c801a13d
Siddharthxyz <siddharth2003dey@gmail.com>
2025-12-29 20:28:45 +0530

    Fix DB initialization for Render

--- app.py
+++ app.py
@@ -119,7 +119,9 @@ def delete_product(product_id):
 
 
 # ---------------- RUN APP ----------------
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+    app.run()
+
